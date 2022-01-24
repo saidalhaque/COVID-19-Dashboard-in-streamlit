a2bcc921099c71709c4232c0c85a4642289817cd
Said AL Haque <44219449+saidalhaque@users.noreply.github.com>
2022-01-24 12:45:57 +0600

    Update arnob.py

--- arnob.py
+++ arnob.py
@@ -10,7 +10,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import altair as alt
-import plotly.express as px
+import plotly as plt
 
 # Draw a title and some text to the app:
 '''
